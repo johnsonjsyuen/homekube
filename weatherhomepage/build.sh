4501bdb75cc8030b71aeabcdcf6fa19f7e0d5081
johnson yuen <j@johnsonyuen.com>
2026-01-17 23:19:44 +1100

    update

--- weatherhomepage/build.sh
+++ weatherhomepage/build.sh
@@ -5,3 +5,4 @@ set -eu
 docker build . -t localhost:5000/weather-service:latest
 docker push localhost:5000/weather-service:latest
 kubectl apply -f weatherhomepage/weather-deployment.yaml
+kubectl rollout restart deployment weather-service
